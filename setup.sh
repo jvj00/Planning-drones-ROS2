5478d8bfb24eab2a0d7bc7071da892f6c9b4b86e
JvJ00 <manuel.righele@studenti.unitn.it>
2022-06-28 17:51:38 +0200

    fixed script setup

--- setup.sh
+++ setup.sh
@@ -6,7 +6,6 @@ echo "Set absolute path..."
 change_from="path_extern=\"/home/drone/dev_ws/src/py_planner/extern\""
 change_to="path_extern=\"$(pwd)/py_planner/extern\""
 echo $change_to
-sed -i 's/$change_from/$change_to/' py_planner/py_planner/planner_service.py
-sed -i 's/$change_from/$change_to/' py_planner/py_planner/planner_client.py
-
+sed -i "s+$change_from+$change_to+" py_planner/py_planner/planner_service.py
+sed -i "s+$change_from+$change_to+" py_planner/py_planner/planner_client.py
 echo "Done!"
