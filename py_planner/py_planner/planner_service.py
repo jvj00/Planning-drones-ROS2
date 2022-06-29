943aaf015a01285206db74d3ebed8dc38a470d69
JvJ00 <manuel.righele@studenti.unitn.it>
2022-06-29 16:43:43 +0200

    service done

--- py_planner/py_planner/planner_service.py
+++ py_planner/py_planner/planner_service.py
@@ -1,3 +1,4 @@
+from select import EPOLL_CLOEXEC
 from planner_interface.srv import MsgJson
 
 import rclpy
@@ -6,6 +7,7 @@ import json
 import os
 from subprocess import Popen, PIPE
 import time
+import math
 
 ## INPUT FIXED VARIABLE ##
 LEVELS = 3
@@ -21,6 +23,16 @@ path_json=path_extern+"/map_files/points.json"
 path_plan=path_extern+"/plan"
 path_popf=path_extern+"/popf/popf2/popf3-clp"
 path_solutions=path_extern+"/solutions"
+
+def min_distance(targets, point):
+        dist_min=99999999
+        min_target=None
+        for target in targets:
+            dist=math.sqrt(math.pow(target["x"]-point["x"],2)+math.pow(target["y"]-point["y"],2)+math.pow(target["z"]-point["z"],2))
+            if dist<dist_min:
+                dist_min=dist
+                min_target=target
+        return min_target
 
 class Service(Node):
 
@@ -57,11 +69,61 @@ class Service(Node):
             time.sleep(1)
         process.kill()
 
-        ## Read output popf
+        ## Read output popf & format output with json
+        actions={"actions": []}
+        f_sol = open(path_solutions+'/'+os.listdir(path_solutions)[len(os.listdir(path_solutions))-1], "r")
+        f_points = open(path_json)
+        points = json.loads(f_points.read())
+        for line in f_sol:
+            if line.startswith(";"):
+                continue
+            words=line[line.find("(")+1:line.find(")")].split()
+            dict={
+                "action": 0,
+                "drone": "",
+                "x": 0,
+                "y": 0,
+                "z": 0
+            }
+            if(words[0]=="move_drone"):
+                dict["action"]=1
+                dict["drone"]=words[1]
+                element_point=[point for point in points["points"] if point['name']==words[3]][0]
+                dict["x"]=element_point["x"]
+                dict["y"]=element_point["y"]
+                dict["z"]=element_point["z"]
+            if(words[0]=="move_agv"):
+                dict["action"]=2
+                element_point=[point for point in points["points"] if point['name']==words[2]][0]
+                dict["x"]=element_point["x"]
+                dict["y"]=element_point["y"]
+            if(words[0]=="takeoff"):
+                dict["action"]=3
+                dict["drone"]=words[1]
+                element_point=[point for point in points["points"] if point['name']==words[3]][0]
+                dict["x"]=element_point["x"]
+                dict["y"]=element_point["y"]
+                dict["z"]=element_point["z"]
+            if(words[0]=="take_picture"):
+                #with reposition in right place
+                dict["action"]=5
+                dict["drone"]=words[1]
+                element_point=[point for point in points["points"] if point['name']==words[2]][0]
+                dict["x"]=element_point["x"]
+                dict["y"]=element_point["y"]
+                dict["z"]=element_point["z"]
+                ret=min_distance(input["targets"], dict)
+                dict["x"]=ret["x"]
+                dict["y"]=ret["y"]
+                dict["z"]=ret["z"]
+            
+            actions["actions"].append(dict)
 
+        f_sol.close()
+        f_points.close()
 
-        ## Format output with json & produce a response
-
+        ## Produce a response
+        response.out_json = json.dumps(actions)
 
         ## Remove files & return response
         os.remove(path_problem)
@@ -69,7 +131,6 @@ class Service(Node):
         for file in os.listdir(path_solutions):
             os.remove(path_solutions+'/'+file)
 
-        response.out_json = "None"
         return response
 
 
