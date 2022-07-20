a7f1beea00898c99a3287727aba7acd22f78b304
JvJ00 <manuel.righele@studenti.unitn.it>
2022-07-20 13:36:01 +0200

    Fix setup.sh file with new commands

--- setup.sh
+++ setup.sh
@@ -1,13 +1,19 @@
 #!/bin/sh
-echo "Give exec permissions..."
+echo "Giving exec permissions..."
 sudo chmod 777 -R py_planner/extern
-echo "Done!"
-echo "Set absolute path..."
+
+echo "Setting absolute path..."
 change_from="path_extern=\"/home/drone/dev_ws/src/py_planner/extern\""
 change_to="path_extern=\"$(pwd)/py_planner/extern\""
 echo $change_to
 sed -i "s+$change_from+$change_to+" py_planner/py_planner/planner_service.py
 sed -i "s+$change_from+$change_to+" py_planner/py_planner/planner_client.py
-echo "Installing correct setup tools from pip..."
+
+echo "Installing dependecies..."
 pip install setuptools==58.2.0
+sudo apt-get -y install coinor-cbc coinor-libcbc-dev
+
+echo "Making directories..."
+mkdir py_planner/extern/solutions
+
 echo "Done!"
